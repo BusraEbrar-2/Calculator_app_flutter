2298b6778f4fadb217f557f0a32fe1cc4412b7e1
BusraEbrar2 <ozturkbusraebrar@gmail.com>
2024-12-18 01:07:55 +0300

    Calculator
    Bu kod, Flutter ile yapılan ve kullanıcıların iki sayı ile toplama, çıkarma, çarpma ve bölme işlemleri yapabilen basit bir hesap makinesi uygulamasıdır.

---
+++ calculator/lib/anaekran.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+
+
+class AnaEkran extends StatefulWidget {
+  
+
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+
+
+class _AnaEkranState extends State<AnaEkran> {
+ num sayi1 = 0, sayi2 = 0, sonuc = 0;
+
+  
+
+
+
+  TextEditingController t1 = TextEditingController() ;
+  // isim vermek için textfield içini okumak için 
+  TextEditingController t2 = TextEditingController() ;
+
+  sayiTopla(){
+setState(() {
+ sayi1 = num.parse( t1.text );
+ sayi2 = num.parse(t2.text) ;
+  sonuc = sayi1+sayi2 ;
+});
+  }
+   void sayiCikar() {
+    setState(() {
+    sonuc = sayi1-sayi2 ;
+
+    });
+  }
+  void sayiCarp() {
+    setState(() {
+     sonuc =  sayi1 * sayi2;
+    });
+  }
+void sayiBol() {
+    setState(() {
+      if (sayi2 != 0) {
+        sonuc =  sayi1 / sayi2;
+      } else {
+        sonuc = double.nan; // Bölünemez hatası
+      }
+    });
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  Widget build(BuildContext context) {
+    return Container  ( 
+      margin :EdgeInsets.all(50.0) ,
+      child : Center (
+        child:Column(
+          children: [
+            Text ("$sonuc"),
+         TextField(
+          controller:t1 ,
+         ),// kullancıdan sayı girişşi almak için 
+         TextField(
+          controller: t2,
+         ),
+         Row (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+         ElevatedButton(onPressed:sayiTopla,child: Text("Topla"), ),
+                  ElevatedButton(onPressed:sayiCikar,child: Text("Cıkar"), ),
+                           ElevatedButton(onPressed:sayiCarp,child: Text("Carp"), ),
+                                    ElevatedButton(onPressed:sayiBol,child: Text("Böl"), ),
+         ],)
+
+
+
+
+          ],
+        ),)
+
+
+    );
+  }
+}
